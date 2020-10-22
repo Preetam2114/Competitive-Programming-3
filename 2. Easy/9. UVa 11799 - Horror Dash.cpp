9855cfe0fe8ca938032fba1ebe5f9971ab113838
Preetam2114 <preetamrane2114@gmail.com>
2020-10-22 19:19:09 +0530

    UVa 11799 - Horror Dash

---
+++ 2. Easy/9. UVa 11799 - Horror Dash.cpp
@@ -0,0 +1,52 @@
+/*
+ * Chef        : preetam rane
+ * Chef-Id     : pvr2114
+ * Dish        : 9. UVa 11799 - Horror Dash
+ * Created on  : Thursday, 22nd October 2020 6:02:37 pm
+ */
+
+
+#include <bits/stdc++.h>
+#define F first
+#define S second 
+#define PB push_back 
+#define MP make_pair
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define inputarr(arr,n) for(int i = 0; i < n; i++) cin>>arr[i];
+#define inputmat(arr,a,b) for(int i=0; i < a; i++)for(int j=0; j < b; j++)cin>>arr[i][j];
+#define print(arr) for(auto a:arr) cout<<a<<" "; cout<<"\n";
+#define endl '\n'
+
+template<typename KeyType, typename ValueType> 
+std::pair<KeyType,ValueType> get_max( const std::map<KeyType,ValueType>& x ) {
+  using pairtype=std::pair<KeyType,ValueType>; 
+  return *std::max_element(x.begin(), x.end(), [] (const pairtype & p1, const pairtype & p2) {
+        return p1.second < p2.second;
+  }); 
+}
+
+constexpr unsigned int shash(const char *s, int off = 0) {                        
+    return !s[off] ? 5381 : (shash(s, off+1)*33) ^ s[off];                           
+}
+
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    int n,m;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        int a[m];
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[j];
+        }
+        cout<<"Case "<<i+1<<": "<<*max_element(a,a+m)<<endl;
+    }
+	return 0;
+}
