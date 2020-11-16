8d1df6ff62238b6bdd459d7b4a9b224cc40bc1cd
Preetam2114 <preetamrane2114@gmail.com>
2020-11-16 08:31:56 +0530

    Create D.cpp

---
+++ Contests/The 2016 ACM-ICPC Asia Dalian Regional/D.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef preetam
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#endif
+
+#define F first
+#define S second 
+#define pb push_back 
+#define mp make_pair
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define inputarr(arr,n) for(int i = 0; i < n; i++) cin>>arr[i];
+#define inputmat(arr,a,b) for(int i=0; i < a; i++)for(int j=0; j < b; j++)cin>>arr[i][j];
+#define print(arr) for(auto a:arr) cout<<a<<" "; cout<<"\n";
+#define all(ar) ar.begin(), ar.end()
+#define foreach(it, ar) for (auto it = ar.begin(); it != ar.end(); it++)
+#define fil(ar, val) memset(ar, val, sizeof(ar))
+#define endl '\n'
+
+#ifndef preetam
+template<typename KeyType, typename ValueType> 
+std::pair<KeyType,ValueType> get_max( const std::map<KeyType,ValueType>& x ) {
+  using pairtype=std::pair<KeyType,ValueType>; 
+  return *std::max_element(x.begin(), x.end(), [] (const pairtype & p1, const pairtype & p2) {
+        return p1.second < p2.second;
+  }); 
+}
+
+template <class Container>
+void split(const std::string& str, Container& cont){
+    std::istringstream iss(str);
+    std::copy(std::istream_iterator<std::string>(iss),
+         std::istream_iterator<std::string>(),
+         std::back_inserter(cont));
+}
+
+constexpr unsigned int shash(const char *s, int off = 0){                        
+    return !s[off] ? 5381 : (shash(s, off+1)*33) ^ s[off];                           
+}
+
+typedef long long ll;
+typedef long double ld;
+typedef long double f80;
+typedef pair<int, int> pii;
+const ll mod = 1e9;
+#endif
+
+ll gcd(ll a, ll b) {
+    if (!a || !b)
+        return a | b;
+    unsigned shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b)
+            swap(a, b);
+        b -= a;
+    } while (b);
+    return a << shift;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    int p, q;
+    while(scanf("%d %d\n", &p, &q) != EOF){
+        ll hcf = gcd(p,q);
+        ll prod = q/hcf;
+        ll d = pow((p/hcf),2)-(4*prod);
+        if(d<0)
+            cout<<"No Solution"<<endl;
+        else{
+            ll r1 = ((-p/hcf)-sqrt(d))/2;
+            ll r2 = ((-p/hcf)+sqrt(d))/2;
+            cout<<min(abs(r2*hcf),abs(r1*hcf))<<" "<<max(abs(r2*hcf),abs(r1*hcf))<<endl;
+        }
+    }
+	return 0;
+}
