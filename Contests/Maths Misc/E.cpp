4e184685a63e500ac4824de8c37097c4bc7e2f6b
Preetam2114 <preetamrane2114@gmail.com>
2020-11-19 23:51:27 +0530

    UVA 591 Box of Bricks

---
+++ Contests/Maths Misc/E.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef preetam
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#endif
+
+#define F first
+#define S second 
+#define pb push_back 
+#define mp make_pair
+#define rep(i,a,b) for(int i = a; i <= b; i++)
+#define inputarr(arr,n) for(int i = 0; i < n; i++) cin>>arr[i];
+#define inputmat(arr,a,b) for(int i=0; i < a; i++)for(int j=0; j < b; j++)cin>>arr[i][j];
+#define print(arr) for(auto a:arr) cout<<a<<" "; cout<<"\n";
+#define all(ar) ar.begin(), ar.end()
+#define foreach(it, ar) for (auto it = ar.begin(); it != ar.end(); it++)
+#define fil(ar, val) memset(ar, val, sizeof(ar))
+#define endl '\n'
+
+#ifndef preetam
+template<typename KeyType, typename ValueType> 
+std::pair<KeyType,ValueType> get_max( const std::map<KeyType,ValueType>& x ) {
+  using pairtype=std::pair<KeyType,ValueType>; 
+  return *std::max_element(x.begin(), x.end(), [] (const pairtype & p1, const pairtype & p2) {
+        return p1.second < p2.second;
+  }); 
+}
+
+template <class Container>
+void split(const std::string& str, Container& cont){
+    std::istringstream iss(str);
+    std::copy(std::istream_iterator<std::string>(iss),
+         std::istream_iterator<std::string>(),
+         std::back_inserter(cont));
+}
+
+constexpr unsigned int shash(const char *s, int off = 0){                        
+    return !s[off] ? 5381 : (shash(s, off+1)*33) ^ s[off];                           
+}
+
+typedef long long ll;
+typedef long double ld;
+typedef long double f80;
+typedef pair<int, int> pii;
+const ll mod = 1e9;
+#endif
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+      #ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+	ll t=1;
+	cin >> t;
+  int setnumber = 1;
+	while(t!=0){
+		// Code here
+    vector<int> h(t);
+    int avg = 0;
+    int res = 0;
+    for(int i=0;i<t;i++){
+      cin>>h[i];
+      avg += h[i];
+    }
+    avg /= t;
+    for(int n: h){
+      res += (n > avg) ? n-avg : 0;
+    }
+    cout<<"Set #"<< setnumber<< endl<<"The minimum number of moves is "<< res << "."<<endl<<endl; 
+    setnumber++;
+    cin>>t;
+	}
+	return 0;
+}
