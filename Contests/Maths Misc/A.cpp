d99c20319a8d5d03e1ab054d15e9f4b38b8bbca0
Preetam2114 <preetamrane2114@gmail.com>
2020-11-19 23:51:06 +0530

    UVA 113 Power of Cryptography

---
+++ Contests/Maths Misc/A.cpp
@@ -0,0 +1,52 @@
+/////////////////////// SET-UP ///////////////////////
+#include<bits/stdc++.h>
+#include <chrono> 
+#include <unistd.h> 
+#include <sys/resource.h>
+#include <errno.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+typedef long double ld;
+typedef long double f80;
+typedef pair<int, int> pii;
+const ll mod = 1e9;
+auto start = high_resolution_clock::now();  
+///////////////////// CODE HERE ///////////////////////
+
+
+//////////////////// ONLINE_JUDGE ////////////////////
+int main(){ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+    struct rusage r_usage;
+    int ret = getrusage(RUSAGE_SELF,&r_usage);
+///////////////////// CODE HERE ///////////////////////
+    ld n,p,ans;
+    while(scanf("%Lf %Lf",&n,&p)==2){
+    	ans=pow(p,1/n);
+        printf("%.0Lf\n",ans);
+    } 
+//////////////////// System log //////////////////////
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout<<"----------------------------------------"<<endl;
+    std::cout << std::fixed << std::showpoint;
+    std::cout << std::setprecision(4);
+    cout << "Execution Time :  "<<float(duration.count()/1000000)
+            << " (seconds)" << endl;
+    cout<< "Process id     :  " 
+             << getpid() << endl;
+    cout<< "Memory usage   :  "<<r_usage.ru_maxrss<<endl;
+    cout<<"----------------------------------------"<<endl;
+//////////////////////////////////////////////////////
+    return 0; 
+} 
