9f9077d67e22f9e7b2cc41dde1a7663734a8c0cc
Preetam2114 <preetamrane2114@gmail.com>
2020-11-19 23:54:03 +0530

    UVA 846 Steps

---
+++ Contests/Maths Misc/H.cpp
@@ -0,0 +1,71 @@
+/*
+ * Chef        : preetam rane
+ * Chef-Id     : pvr2114
+ * Dish        : H
+ * Created on  : Thursday, 19th November 2020 9:01:54 pm
+ */
+
+
+/////////////////////// SET-UP ///////////////////////
+#include<bits/stdc++.h>
+#include <chrono> 
+#include <unistd.h> 
+#include <sys/resource.h>
+#include <errno.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace std::chrono;
+
+typedef long long ll;
+typedef long double ld;
+typedef long double f80;
+typedef pair<int, int> pii;
+const ll mod = 1e9;
+auto start = high_resolution_clock::now();  
+///////////////////// CODE HERE ///////////////////////
+
+
+//////////////////// ONLINE_JUDGE ////////////////////
+int main(){ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+	    freopen("input.txt","r",stdin);
+	    freopen("output.txt","w",stdout);
+    #endif
+    struct rusage r_usage;
+    int ret = getrusage(RUSAGE_SELF,&r_usage);
+///////////////////// CODE HERE ///////////////////////
+    ll t=1;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int steps = y-x;
+        int count=0;
+        if(steps == 1)
+            cout<<1<<endl;
+        else if (steps==2){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<ceil(sqrt(4*steps))-1<<endl;
+        }
+    } 
+//////////////////// System log //////////////////////
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout<<"----------------------------------------"<<endl;
+    std::cout << std::fixed << std::showpoint;
+    std::cout << std::setprecision(4);
+    cout << "Execution Time :  "<<float(duration.count()/1000000)
+            << " (seconds)" << endl;
+    cout<< "Process id     :  " 
+             << getpid() << endl;
+    cout<< "Memory usage   :  "<<r_usage.ru_maxrss<<endl;
+    cout<<"----------------------------------------"<<endl;
+//////////////////////////////////////////////////////
+    return 0; 
+} 
